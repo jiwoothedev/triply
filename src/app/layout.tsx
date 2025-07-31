ee956abc74e6b74027c51c45b72f6eefc7d23964
Jiwoo <jiwoothedev@gmail.com>
2025-07-31 19:49:01 +0900

    chore: 컴포넌트 폴더 구조 정리 및 import 경로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,27 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "@next/font/google";
-import Layout from "@/components/Layout";
+import Layout from "@/components/common/Layout";
 
 // Inter 폰트
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 // 메타 데이터
 export const metadata: Metadata = {
-    title: "TRIPLY",
-    // TODO: 추가 예정
+  title: "TRIPLY",
+  // TODO: 추가 예정
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="ko" className={`${inter.variable}`}>
-            <body>
-                <Layout>{children}</Layout>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="ko" className={`${inter.variable}`}>
+      <body>
+        <Layout>{children}</Layout>
+      </body>
+    </html>
+  );
 }
