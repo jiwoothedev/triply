95484c6ac641183f6ae72374540b294fd1007ea6
Jiwoo <jiwoothedev@gmail.com>
2025-08-03 23:51:35 +0900

    feat: CommunityCard 컴포넌트 추가

---
+++ src/components/main/CommunityCard.tsx
@@ -0,0 +1,24 @@
+import Button from "../common/Button";
+import FlagIcon from "./FlagIcon";
+
+interface CommunityCardProps {
+  content: string;
+}
+
+export default function CommunityCard({ content }: CommunityCardProps) {
+  return (
+    <div className="w-[320px] h-[430px] rounded-2xl p-5 bg-primary flex flex-col justify-center">
+      <FlagIcon country="Canada" />
+      <p className="mt-8 mb-32 font-semibold text-primary-dark text-sm">
+        {content}
+      </p>
+      <Button
+        width="w-[280px]"
+        text="리뷰 더보기"
+        textSize="text-sm"
+        textColor="text-primary"
+        bgColor="bg-primary-dark hover:brightness-130"
+      />
+    </div>
+  );
+}
