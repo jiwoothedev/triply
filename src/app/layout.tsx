bf04157402f3708ccd334f9d1bd40888154e0699
Jiwoo <jiwoothedev@gmail.com>
2025-08-12 20:11:28 +0900

    chore: next/font import 제거 (Vercel 빌드 에러)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 import Layout from "@/components/common/Layout";
 
 // Inter 폰트
