aa2c6ecac93f0d0b76cdecdb6680057baf307ef9
Jiwoo <jiwoothedev@gmail.com>
2025-08-25 22:58:50 +0900

    style: MainCommunityCard 레이아웃 수정

--- src/components/main/MainCommunityCard.tsx
+++ src/components/main/MainCommunityCard.tsx
@@ -7,18 +7,24 @@ interface MainCommunityCardProps {
 
 export default function MainCommunityCard({ content }: MainCommunityCardProps) {
   return (
-    <div className="w-[320px] h-[430px] rounded-2xl p-5 bg-primary flex flex-col justify-center">
-      <FlagIcon country="Canada" />
-      <p className="mt-8 mb-32 font-semibold text-primary-dark text-sm">
-        {content}
-      </p>
-      <Button
-        width="w-[280px]"
-        text="리뷰 더보기"
-        textSize="text-sm"
-        textColor="text-primary"
-        bgColor="bg-primary-dark hover:brightness-130"
-      />
+    <div className="w-[320px] h-[430px] rounded-2xl p-5 bg-primary flex flex-col justify-between">
+      <div>
+        <FlagIcon country="Canada" />
+      </div>
+
+      <div className="flex-1 flex items-center">
+        <p className="font-semibold text-primary-dark text-sm">{content}</p>
+      </div>
+
+      <div className="flex justify-center">
+        <Button
+          width="w-[280px]"
+          text="리뷰 더보기"
+          textSize="text-sm"
+          textColor="text-primary"
+          bgColor="bg-primary-dark hover:brightness-130"
+        />
+      </div>
     </div>
   );
 }
